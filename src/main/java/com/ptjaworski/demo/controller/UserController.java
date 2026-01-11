0828f9f134e51949d571e1d3f368e5c597f15623
PJaworski <pjaworski.dev@gmail.com>
2026-01-11 20:29:08 +0100

    Add base UserController

--- src/main/java/com/ptjaworski/demo/controller/UserController.java
+++ src/main/java/com/ptjaworski/demo/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.ptjaworski.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ptjaworski.demo.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    @GetMapping
-    public String getUser() {
-        return "User";
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping("/{id}")
+    public String getUserById (@PathVariable Long id) {
+        log.info("getUserById called");
+        return "User " + id;
+
+    }
+
+    @PostMapping("/register")
+    public String postUser(@RequestBody User user) {
+        return "Post User " + user;
+    }
+
+    @PutMapping("/update/{id}")
+    public String updateUser(@PathVariable Long id, @RequestBody User user) {
+        return "Update User ";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "Delete User ";
     }
 
 
