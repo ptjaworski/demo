d2a990049fa40399323bb966d36255f3d3a16887
PJaworski <pjaworski.dev@gmail.com>
2026-01-11 21:42:25 +0100

    Add getAllUsers, userService

--- src/main/java/com/ptjaworski/demo/controller/UserController.java
+++ src/main/java/com/ptjaworski/demo/controller/UserController.java
@@ -1,14 +1,31 @@
 package com.ptjaworski.demo.controller;
 
+import com.ptjaworski.demo.model.User;
 import com.ptjaworski.demo.model.UserEntity;
+import com.ptjaworski.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<User>> getAllUsers() {
+        log.info("getAllUsers called");
+        return ResponseEntity.status(HttpStatus.OK).body(userService.getAllUsers());
+    }
 
     @GetMapping("/{id}")
     public String getUserById (@PathVariable Long id) {
